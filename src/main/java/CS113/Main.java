5fb817794bcc89ba679f3646e365a2bcdaded89c
MCCLab4804WD22 <pkaislasaari@gmail.com>
2025-02-18 20:30:21 -0800

    AlmostDone

--- src/main/java/CS113/Main.java
+++ src/main/java/CS113/Main.java
@@ -2,6 +2,17 @@ package CS113;
 
 public class Main {
     public static void main(String[] args) {
+
+        ArrayListPK<Integer> testArray = new ArrayListPK<>();
+
+        for (int i = 0; i < 10; i++){
+            testArray.add(i);
+        }
+
+        testArray.clear();
+
+        System.out.println(testArray);
+
         System.out.println("Hello world!");
     }
 }
